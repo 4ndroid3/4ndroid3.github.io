2a7b0cd7ca41dea246f7524f7d412bf96223b3df
4ndroid3 <andijaimo@gmail.com>
2021-06-02 22:08:14 -0300

    se prueban hero images

--- js/js.js
+++ js/js.js
@@ -4,31 +4,31 @@ var el2 = document.getElementById("elementos2");
 var el3 = document.getElementById("elementos3");
 var el4 = document.getElementById("elementos4");
 
-el1.addEventListener('mouseover', function (event) {
+el1.addEventListener('mouseover', function(event) {
     event.target.style.color = "#191716ff";
 })
-el1.addEventListener('mouseout', function (event) {
+el1.addEventListener('mouseout', function(event) {
     event.target.style.color = "#a7a2a9ff";
 })
 
-el2.addEventListener('mouseover', function (event) {
+el2.addEventListener('mouseover', function(event) {
     event.target.style.color = "#191716ff";
 })
-el2.addEventListener('mouseout', function (event) {
+el2.addEventListener('mouseout', function(event) {
     event.target.style.color = "#a7a2a9ff";
 })
 
-el3.addEventListener('mouseover', function (event) {
+el3.addEventListener('mouseover', function(event) {
     event.target.style.color = "#191716ff";
 })
-el3.addEventListener('mouseout', function (event) {
+el3.addEventListener('mouseout', function(event) {
     event.target.style.color = "#a7a2a9ff";
 })
 
-el4.addEventListener('mouseover', function (event) {
+el4.addEventListener('mouseover', function(event) {
     event.target.style.color = "#191716ff";
 })
-el4.addEventListener('mouseout', function (event) {
+el4.addEventListener('mouseout', function(event) {
     event.target.style.color = "#a7a2a9ff";
 })
 
@@ -39,9 +39,9 @@ el4.addEventListener('mouseout', function (event) {
 function myFunction1() {
     var x = document.getElementById("projecto1");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
 }
 
